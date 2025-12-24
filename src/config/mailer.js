bfdcd4a9cfc7464852fdb7d586136fd8c991014c
Zain Baig <zain.baig98@gmail.com>
2025-12-24 20:02:39 +0300

    changes

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -5,10 +5,21 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: true,
+  port: Number(process.env.SMTP_PORT),
+  secure: true, // for 465
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    pass: process.env.SMTP_PASS
   },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP ERROR ❌", error);
+  } else {
+    console.log("SMTP Ready ✅");
+  }
 });
--- src/server.js
+++ src/server.js
@@ -5,6 +5,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Lead system running on port ${PORT}`);
 });
